31eb18bcd9668f916713b8ad2d098f1779029e3e
janelle mellor <janellemellor@gmail.com>
2020-02-25 10:26:36 -0800

    add form and link to form from home page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import Home from './Home.js';
 import Detail from './Detail.js';
+import CreateBoba from './CreateBoba.js';
 
 export default class App extends Component {
   render() {
@@ -15,9 +16,11 @@ export default class App extends Component {
       <Router>
         <div>
           <Link className="page-links" to="/">Return Home</Link>
+          <Link className="page-links" to="/add/create-boba" >Create new boba!</Link>
           <Switch>
             <Route exact path="/:bobaData?" component={Home} />
             <Route exact path="/detail/:bobaId" component={Detail} />
+            <Route exact path="/add/create-boba" component={CreateBoba} ></Route>
           </Switch>
         </div>  
       </Router>    
--- src/CreateBoba.js
+++ src/CreateBoba.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import { getTypesData, postBobaData } from './bobaAPI.js';
 
 export default class CreateBoba extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
+        return (        
+            <fieldset>   
+                <legend>
+                    <h3>Create your own Boba!</h3>
+                    <form>
+                        <label>
+                            Flavor:
+                            <input>
+                            </input>
+                        </label>
+
+                        <label>
+                            Type:
+                            <input>
+                            </input>
+                        </label>
+
+                        <label>
+                            Is Milk Tea?
+                            <input>
+                            </input>
+                        </label>
+
+                        <label>
+                            Image:
+                            <input>
+                            </input>
+                        </label>
+
+                        <label>
+                            Star Rating
+                            <input>
+                            </input>
+                        </label>
+
+                        <button>Create!</button>
+
+                    </form>
+                </legend>
+            </fieldset>     
         )
     }
 }
